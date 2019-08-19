f6ef83d27f3fc96e7e10403764f37714dd6b08ab
chazheng <1325520624@qq.com>
2019-08-19 16:06:00 +0800

    新建springboot项目

--- src/main/java/com/example/git123/controller/MyController.java
+++ src/main/java/com/example/git123/controller/MyController.java
@@ -2,6 +2,8 @@ package com.example.git123.controller;
 
 import com.example.git123.entity.User;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,15 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/web")
 public class MyController {
-    @RequestMapping("/login")
-    public String login(User user){
+    /**
+     * get请求参数处理 2019/8/19
+     * @param username
+     * @param pwd
+     * @return
+     */
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(@RequestParam(value = "username",defaultValue = "zcc",required = false)String username,
+                        @RequestParam(value = "pwd",required = true)String pwd){
         /**
          * @author zcc
          * 修改 加入判断登录条件
          * @date 2019/8/19
          */
         String msg="登录成功";
-        if("zcc".equals(user.getUsername())&&"1234".equals(user.getPwd())){
+        if("zcc".equals(username)&&"1234".equals(pwd)){
             return msg;
         }else{
             msg="登录失败";
